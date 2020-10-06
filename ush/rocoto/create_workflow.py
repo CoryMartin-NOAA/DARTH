ecd0af3b913cf3dd177795ffb46e6629124075d0
Cory Martin <cory.r.martin@noaa.gov>
2020-10-05 21:16:57 -0400

    Some more rocoto work; saving for the night

--- ush/rocoto/create_workflow.py
+++ ush/rocoto/create_workflow.py
@@ -15,6 +15,63 @@ def main():
     args = parser.parse_args()
     create_workflow(args.yaml, args.xml)
 
+def get_preamble():
+    '''
+        Generate preamble for XML
+    '''
+
+    strings = []
+
+    strings.append('<?xml version="1.0"?>\n')
+    strings.append('<!DOCTYPE workflow\n')
+    strings.append('[\n')
+    strings.append('\t<!--\n')
+    strings.append('\tPROGRAM\n')
+    strings.append('\t\tMain workflow manager for DARTH\n')
+    strings.append('\n')
+    strings.append('\tAUTHOR:\n')
+    strings.append('\t\tCory Martin\n')
+    strings.append('\t\tcory.r.martin@noaa.gov\n')
+    strings.append('\n')
+    strings.append('\tadapted from global-workflow rocoto scripts by Rahul Mahajan\n')
+    strings.append('\n')
+    strings.append('\t-->\n')
+
+    return ''.join(strings)
+
+def get_workflow_footer():
+    '''
+        Generate workflow footer
+    '''
+
+    strings = []
+    strings.append('\n</workflow>\n')
+
+    return ''.join(strings)
+
+def create_entities(yamlconfig):
+    machine = wfu.detectMachine()
+    scheduler = wfu.get_scheduler(machine)
+    strings = []
+    strings.append('\n')
+    strings.append('\t<!-- Experiment parameters such as name, starting, ending dates -->\n')
+    strings.append('\t<!ENTITY PSLOT "DARTH">\n')
+    strings.append('\t<!ENTITY SDATE "%s">\n' % yamlconfig['DARTH']['sdate'])
+    strings.append('\t<!ENTITY EDATE "%s">\n' % yamlconfig['DARTH']['edate'])
+    strings.append('\t<!-- Machine related entities -->\n')
+    strings.append('\t<!ENTITY ACCOUNT    "%s">\n' % yamlconfig['account'])
+
+    strings.append('\t<!ENTITY QUEUE      "%s">\n' % yamlconfig['queue'])
+    strings.append('\t<!ENTITY SCHEDULER  "%s">\n' % scheduler)
+    strings.append('\t<!ENTITY ROOTWORK "%s">\n' % yamlconfig['paths']['rootdir'])
+    strings.append('\n')
+    strings.append('\t<!-- ROCOTO parameters that control workflow -->\n')
+    strings.append('\t<!ENTITY CYCLETHROTTLE "3">\n')
+    strings.append('\t<!ENTITY TASKTHROTTLE  "25">\n')
+    strings.append('\t<!ENTITY MAXTRIES      "2">\n')
+    strings.append('\n')
+    return ''.join(strings)
+
 def create_workflow(yamlpath, xmlpath):
     # read in YAML file for configuration
     with open(yamlpath, 'r') as stream:
@@ -25,6 +82,22 @@ def create_workflow(yamlpath, xmlpath):
     # loop through tasks
     for task in tasks:
         taskresources = yamlconfig[task]
+
+
+    # setup the XML
+    preamble = get_preamble()
+    workflow_footer = get_workflow_footer()
+    xmlfile = []
+    xmlfile.append(preamble)
+    xmlfile.append(create_entities(yamlconfig))
+    xmlfile.append(workflow_footer)
+
+    # Write the XML file
+    fh = open(xmlpath, 'w')
+    fh.write(''.join(xmlfile))
+    fh.close()
+
+    return
 
 
 if __name__ == '__main__':
