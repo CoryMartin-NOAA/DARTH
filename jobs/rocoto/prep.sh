6757021af9b7b1a785560e1a03ab99209cc49f18
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2020-10-08 20:35:24 +0000

    prep step part of rocoto seemingly working

--- jobs/rocoto/prep.sh
+++ jobs/rocoto/prep.sh
@@ -6,13 +6,16 @@ env
 # load modules used by all jobs; for python3, etc.
 source $HOMEDARTH/ush/load_modules_darth.sh || exit 1
 
-# generate the YAML for this cycle
 adate=${PDY}${cyc}
-mkdir -p $ROTDIR/$adate/$YAML
+
+# create subdirectories for this cycle
+mkdir -p $ROTDIR/$adate/YAML
+
+# generate the YAML for this cycle
 python $HOMEDARTH/scripts/gen_cycle_yaml.py -y $TOPYAML -d $adate -o $ROTDIR/$adate/YAML/DARTH_cycle_${adate}.yaml || exit 1
 
 # generate the YAML for all subsequent jobs
-python $HOMEDARTH/setup_proc_cycle.py -y $ROTDIR/$adate/YAML/DARTH_cycle_${adate}.yaml || exit 1
+python $HOMEDARTH/scripts/setup_proc_cycle.py -y $ROTDIR/$adate/YAML/DARTH_cycle_${adate}.yaml || exit 1
 
 date
 echo "prep.sh completed"
--- scripts/gen_cycle_yaml.py
+++ scripts/gen_cycle_yaml.py
@@ -9,11 +9,18 @@ import argparse
 import yaml
 import datetime
 import re
+import os
 
 def main(yamlconfig, adate, outfile):
     # need to create a YAML file to be read by
-    comrot = yamlconfig['DARTH']['comrot']+'/'+adate.strftime('%Y%m%d%H')
     # setup_proc_cycle.py
+    # figure out some variables
+    comrot = yamlconfig['DARTH']['comrot']+'/'+adate.strftime('%Y%m%d%H')
+    if os.getenv('SLURM_SET','NO') == 'YES':
+       launcher = 'srun --export=ALL'
+    else:
+       launcher = 'mpirun -np ' #TODO finish this for non-slurm
+    threads = 1
     yamlout = {}
     # things for this cycle specifically
     timestr = adate.strftime('%Y-%m-%dT%H:00:00Z')
@@ -32,21 +39,60 @@ def main(yamlconfig, adate, outfile):
              'format': yamlconfig['background']['format'],
              'gfsv16': yamlconfig['background']['gfsv16'],
     }
+    gsienv = {
+             'launcher': launcher,
+             'nthreads': threads,
+    }
     yamlout['gsi observer'] = {
                               'background': gsibkg,
                               'observations': {'bufrdir': yamlconfig['paths']['obsdir']},
                               'gsidir': yamlconfig['paths']['gsiroot'],
                               'gsiwork': comrot+'/GSI_work/',
                               'gsiout': comrot+'/GSI_out/',
+                              'env': gsienv,
     }
     # YAML for the JEDI H(x) step
+    jedibkg = {
+               'res': yamlconfig['background']['res'],
+               'levs': yamlconfig['background']['levs'],
+               'guessdir': yamlconfig['paths']['guessdir'],
+               'gfsv16': yamlconfig['background']['gfsv16'],
+    }
+    jedienv = {
+             'launcher': launcher,
+             'nthreads': threads,
+             'modulefile': yamlconfig['paths']['jedimodule'],
+    }
     yamlout['jedi hofx'] = {
+        'background': jedibkg,
+        'observations': {'iodadir': comrot+'/IODA/'},
+        'env': jedienv,
+        'hofxwork': comrot+'/JEDI_work/',
+        'hofxout': comrot+'/JEDI_hofx/',
+        'executable': yamlconfig['paths']['jedibin'] + '/fv3jedi_hofx_nomodel.x',
+        'fixfv3jedi': yamlconfig['paths']['jediroot'] + '/fv3-jedi/test/Data',
     }
     # YAML for the conversion step
+    iodaenv = {
+             'launcher': launcher,
+             'nthreads': threads,
+             'modulefile': yamlconfig['paths']['jedimodule'],
+    }
     yamlout['ioda-converters'] = {
+            'iodaconvbin': yamlconfig['paths']['iodaconvbin'] + '/proc_gsi_ncdiag.py',
+            'iodaconvwork': comrot+'/IODA_work/',
+            'gsidiagdir': comrot+'/GSI_out/',
+            'iodaout': comrot+'/IODA/',
+            'env': iodaenv,
     }
     # other misc things
     yamlout['cleanup'] = yamlconfig['DARTH']['cleanup']
+    yamlout['yamldir'] = comrot + '/YAML/'
+
+    # write out the YAML file
+    with open(outfile, 'w') as file:
+        yaml.dump(yamlout, file, default_flow_style=False)
+    print('YAML written to: '+outfile)
 
 
 parser = argparse.ArgumentParser(description='Generate YAML for this global cycle which is then'+\
