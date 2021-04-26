8b0026e2c714a926340497ccb50bc2c636d8144d
Cory Martin <cory.r.martin@noaa.gov>
2021-04-26 17:30:12 -0400

    Working basic example using the verification group templates

--- diags/DARTHsite.py
+++ diags/DARTHsite.py
@@ -5,83 +5,6 @@ import datetime
 
 ozdiag = ['omps_npp']  # fill out later
 
-def genNavPanel(cyclelist, templatefile, outfile):
-    # read in template and generate NavPanel
-    with open(templatefile) as htmlin:
-        cycleconvtable = ''
-        cycleconvplots = ''
-        cycleradtable = ''
-        cycleradplots = ''
-        for cycle in cyclelist:
-            cycleconvtable = cycleconvtable + f'<OPTION VALUE="{cycle}_convtable.html" selected >{cycle}</option>\n'
-            cycleconvplots = cycleconvplots + f'<OPTION VALUE="{cycle}_convplots.html" selected >{cycle}</option>\n'
-            cycleradtable = cycleradtable + f'<OPTION VALUE="{cycle}_radtable.html" selected >{cycle}</option>\n'
-            cycleradplots = cycleradplots + f'<OPTION VALUE="{cycle}_radplots.html" selected >{cycle}</option>\n'
-        replacements = {
-            '{{CYCLECONVTABLE}}': cycleconvtable,
-            '{{CYCLECONVPLOTS}}': cycleconvplots,
-            '{{CYCLERADTABLE}}': cycleradtable,
-            '{{CYCLERADPLOTS}}': cycleradplots,
-        }
-        with open(outfile, 'w') as htmlout:
-            for line in htmlin:
-                for src, target in replacements.items():
-                    line = line.replace(src,target)
-                htmlout.write(line)
-
-def genMainPanel(cyclelist, templatefile, outfile, expname):
-    # read in template and generate main homepage panel
-    with open(templatefile) as htmlin:
-        cyclehtml = ''
-        for cycle in cyclelist:
-            cyclehtml = cyclehtml + f'{cycle}<br>\n'
-        timestr = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
-        replacements = {
-            '{{CYCLES}}': cyclehtml,
-            '{{EXPERIMENT}}': expname,
-            '{{GENTIME}}': timestr,
-        }
-        with open(outfile, 'w') as htmlout:
-            for line in htmlin:
-                for src, target in replacements.items():
-                    line = line.replace(src,target)
-                htmlout.write(line)
-
-def genDataRow(valuelist, minlist, maxlist):
-    # create a row of a table in HTML
-    # format color of row depending on value/content of column
-    print(valuelist)
-
-def genConvTable(cycle, ConvDict, outpath):
-    # generate HTML table of info on conventional data
-    htmlstr = ''
-    htmlstr = htmlstr + f'<html><h1>Conventional Data Summary for {cycle}</h1>\n'
-    htmlstr = htmlstr + '<style>table, th, td { border: 1px solid black; border-collapse: collapse;}</style>'
-    ## create massive table here from input dictionary
-    for ConvType, TypeDict in ConvDict.items():
-        # ConvType being sondes, sfc, etc.
-        # new table for each type
-        newline = f'<h2>{ConvType}</h2>\n'
-        htmlstr = ''.join((htmlstr, newline))
-        columns = TypeDict['columns']
-        minlist = TypeDict['threshold_min']
-        maxlist = TypeDict['threshold_max']
-        newline = '<table style="width:90%"><tr>\n'
-        htmlstr = ''.join((htmlstr, newline))
-        for col in columns:
-            htmlstr = htmlstr + f'<th>{col}</th>\n'
-        htmlstr = htmlstr + '</tr>\n'
-        for row, rowvalues in TypeDict.items():
-            if row in ['columns', 'threshold_min', 'threshold_max']:
-                continue
-            newline = genDataRow(rowvalues, minlist, maxlist)
-            htmlstr = htmlstr + newline
-        htmlstr = htmlstr + '</table>'
-    htmlstr = htmlstr + '</html>'
-    outfile = f'{outpath}/html/{cycle}_convtable.html'
-    with open(outfile, 'w') as htmlout:
-        htmlout.write(htmlstr)
-
 def genConvPlotPage(cycledir, outfile):
     # find all conventional plots in this directory, group them properly,
     # and put them into a basic HTML page for viewing/clicking
@@ -166,24 +89,146 @@ def genRadPlotPage(cycledir, outfile):
     with open(outfile, 'w') as htmlout:
         htmlout.write(htmlstr)
 
+def genRadPage(htmlfile, templatefile, cycles, sensor):
+    # generate main html page for each individual sensor
+    with open(templatefile) as htmlin:
+        # create javascript for all available cycles
+        cychtml = ''
+        for cyc in cycles:
+            cychtml = cychtml + f'validtimes.push({{displayName: "{cyc}", name: "{cyc}"}});\n'
+        # create javascript for all channels
+        chanhtml = ''
+        chans = []
+        for ch in chans:
+            chanhtml = chanhtml + f'channels.push({{displayName: "{ch}", name: "{ch}"}});\n'
+        replacements = {
+            '{{SENSOR}}': sensor,
+            '{{CYCLEPUSH}}': cychtml,
+            '{{CHANNELPUSH}}': chanhtml,
+            '{{CYCLE1}}': cycles[0],
+        }
+        with open(htmlfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src,target)
+                htmlout.write(line)
+
+def genRootIndex(htmldir, templatefile, cycledirs, expname):
+    # create index.html for the top level page from a template
+    with open(templatefile) as htmlin:
+        # get list of all sensors in the cycledirs
+        sensors = []
+        for cycle in cycledirs:
+            dirs = glob.glob(os.path.join(cycle, '*'))
+            for sdir in dirs:
+                sensor = os.path.basename(sdir)
+                if sensor not in sensors:
+                    sensors.append(sensor)
+        # get list of rad, conv, oz sensors for menus
+        radlist = []
+        convlist = []
+        ozlist = []
+        for obtype in sensors:
+            if len(obtype.split('_')) > 1:
+                radlist.append(obtype) if obtype not in ozdiag else ozlist.append(obtype)
+            else:
+                convlist.append(obtype)
+        # create HTML to replace in template
+        rad_html = ''
+        conv_html = ''
+        oz_html = ''
+        for rad in radlist:
+            rad_html = rad_html + f'<a href="rad/{rad}/index.html">{rad.upper()}</a>\n'
+        for conv in convlist:
+            conv_html = conv_html + f'<a href="conv/{conv}/index.html">{conv.upper()}</a>\n'
+        for oz in ozlist:
+            oz_html = oz_html + f'<a href="oz/{oz}/index.html">{oz.upper()}</a>\n'
+        # find and replace in template
+        replacements = {
+            '{{EXPNAME}}': expname,
+            '{{RADLIST}}': rad_html,
+            '{{CONVLIST}}': conv_html,
+            '{{OZLIST}}': oz_html,
+            '{{CONTENTHTML}}': 'main.html',
+            '{{HOMEPATH}}': '',
+        }
+        outfile = os.path.join(htmldir, 'index.html')
+        with open(outfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src,target)
+                htmlout.write(line)
+    return radlist, convlist, ozlist
+
+def genSensorIndex(htmlfile, templatefile, expname, mysensor, radlist, convlist, ozlist):
+    # creates index.html for the individual ob type pages
+    with open(templatefile) as htmlin:
+        # create HTML to replace in template
+        rad_html = ''
+        conv_html = ''
+        oz_html = ''
+        for rad in radlist:
+            rad_html = rad_html + f'<a href="../../rad/{rad}/index.html">{rad.upper()}</a>\n'
+        for conv in convlist:
+            conv_html = conv_html + f'<a href="../../conv/{conv}/index.html">{conv.upper()}</a>\n'
+        for oz in ozlist:
+            oz_html = oz_html + f'<a href="../../oz/{oz}/index.html">{oz.upper()}</a>\n'
+        # find and replace in template
+        replacements = {
+            '{{EXPNAME}}': expname,
+            '{{RADLIST}}': rad_html,
+            '{{CONVLIST}}': conv_html,
+            '{{OZLIST}}': oz_html,
+            '{{CONTENTHTML}}': f'{mysensor}.html',
+            '{{HOMEPATH}}': '../../index.html'
+        }
+        with open(htmlfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src,target)
+                htmlout.write(line)
+
 def genSite(htmldir, templatedir, expname='evaltest'):
     # generate HTML for DARTH site depending on what figures, etc. are in htmldir
     # get list of cycles
     cycledirs = glob.glob(os.path.join(htmldir, 'figs', '*'))
     cycles = [os.path.basename(c) for c in cycledirs]
-    # create index.html if it does not exist
-    index_html = os.path.join(htmldir,'index.html')
-    if os.path.exists(index_html):
-        os.remove(index_html)
-    shutil.copy(os.path.join(templatedir,'index.html'), index_html)
-    templatefile = os.path.join(templatedir, 'navtemplate.html')
-    outfile = os.path.join(htmldir, 'panel.html')
-    genNavPanel(cycles, templatefile, outfile)
-    outfile = os.path.join(htmldir, 'main.html')
-    templatefile = os.path.join(templatedir, 'maintemplate.html')
-    genMainPanel(cycles, templatefile, outfile, expname)
-    for cycle, cycledir in zip(cycles, cycledirs):
-        outfile = os.path.join(htmldir, f'{cycle}_radplots.html')
-        genRadPlotPage(cycledir, outfile)
-        outfile = os.path.join(htmldir, f'{cycle}_convplots.html')
-        genConvPlotPage(cycledir, outfile)
+    # just copy main.html if it does not exist
+    main_html = os.path.join(htmldir,'main.html')
+    if os.path.exists(main_html):
+        os.remove(main_html)
+    shutil.copy(os.path.join(templatedir,'main.html'), main_html)
+    # create top level index.html
+    radlist, convlist, ozlist = genRootIndex(htmldir,
+                                os.path.join(templatedir,'index.html'),
+                                cycledirs,
+                                expname,
+                                )
+    # now for each rad, conv, oz sensor, make a directory/html pages
+    # if not os.path.exists(os.path.join(htmldir, 'rad')):
+    #     os.makedirs(os.path.join(htmldir, 'rad'))
+    # if not os.path.exists(os.path.join(htmldir, 'conv')):
+    #     os.makedirs(os.path.join(htmldir, 'conv'))
+    # if not os.path.exists(os.path.join(htmldir, 'oz')):
+    #     os.makedirs(os.path.join(htmldir, 'oz'))
+    for rad in radlist:
+        if not os.path.exists(os.path.join(htmldir, 'rad', rad)):
+            os.makedirs(os.path.join(htmldir, 'rad', rad))
+        genSensorIndex(os.path.join(htmldir, 'rad', rad, 'index.html'),
+                       os.path.join(templatedir,'index.html'),
+                       expname, rad, radlist, convlist, ozlist)
+        genRadPage(os.path.join(htmldir, 'rad', rad, f'{rad}.html'),
+                   os.path.join(templatedir,'radmain.html'),
+                   cycles, rad)
+    for conv in convlist:
+        if not os.path.exists(os.path.join(htmldir, 'conv', conv)):
+            os.makedirs(os.path.join(htmldir, 'conv', conv))
+        genSensorIndex(os.path.join(htmldir, 'conv', conv, 'index.html'),
+                       os.path.join(templatedir,'index.html'),
+                       expname, conv, radlist, convlist, ozlist)
+    for oz in ozlist:
+        if not os.path.exists(os.path.join(htmldir, 'oz', oz)):
+            os.makedirs(os.path.join(htmldir, 'oz', oz))
+        genSensorIndex(os.path.join(htmldir, 'oz', oz, 'index.html'),
+                       os.path.join(templatedir,'index.html'),
+                       expname, oz, radlist, convlist, ozlist)
