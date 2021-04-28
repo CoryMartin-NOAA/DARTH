eae19dedb6991f34c0091e6eacfd436c71ef61ed
Cory Martin <cory.r.martin@noaa.gov>
2021-04-28 08:16:34 -0400

    Initial merge of batch plots and website (#43)
    Beginning of capability to generate figures in batch for UFO evaluation and create HTML pages from templates to view the figures.

---
+++ diags/DARTHsite.py
@@ -0,0 +1,201 @@
+import glob
+import os
+import shutil
+import datetime
+
+ozdiag = ['omps_npp']  # fill out later
+
+
+def gen_conv_page(htmlfile, templatefile, cycles, cycledirs, obtype):
+    # generate main html page for each individual sensor
+    with open(templatefile) as htmlin:
+        # create javascript for all available cycles
+        cychtml = ''
+        for cyc in cycles:
+            cychtml = cychtml + \
+                f'validtimes.push({{displayName: "{cyc}", name: "{cyc}"}});\n'
+        # create javascript for all variables
+        varhtml = ''
+        my_figs = glob.glob(os.path.join(
+            cycledirs[0], obtype, f'{obtype}_hofx_*_scatter.png'))
+        vars = sorted(['_'.join(os.path.basename(c).split('_')[2:-1])
+                       for c in my_figs])
+        for v in vars:
+            varhtml = varhtml + \
+                f'channels.push({{displayName: "{v}", name: "{v}"}});\n'
+        replacements = {
+            '{{OBTYPE}}': obtype,
+            '{{CYCLEPUSH}}': cychtml,
+            '{{VARPUSH}}': varhtml,
+            '{{CYCLE1}}': cycles[0],
+        }
+        with open(htmlfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src, target)
+                htmlout.write(line)
+
+
+def gen_rad_page(htmlfile, templatefile, cycles, cycledirs, sensor):
+    # generate main html page for each individual sensor
+    with open(templatefile) as htmlin:
+        # create javascript for all available cycles
+        cychtml = ''
+        for cyc in cycles:
+            cychtml = cychtml + \
+                f'validtimes.push({{displayName: "{cyc}", name: "{cyc}"}});\n'
+        # create javascript for all channels
+        chanhtml = ''
+        my_figs = glob.glob(os.path.join(
+            cycledirs[0], sensor, f'{sensor}_hofx_*_scatter.png'))
+        chans = sorted([int(os.path.basename(c).split('_')[-2])
+                        for c in my_figs])
+        for ch in chans:
+            chanhtml = chanhtml + \
+                f'channels.push({{displayName: "{ch}", name: "{ch}"}});\n'
+        replacements = {
+            '{{SENSOR}}': sensor,
+            '{{CYCLEPUSH}}': cychtml,
+            '{{CHANNELPUSH}}': chanhtml,
+            '{{CYCLE1}}': cycles[0],
+        }
+        with open(htmlfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src, target)
+                htmlout.write(line)
+
+
+def gen_root_index(htmldir, templatefile, cycledirs, expname):
+    # create index.html for the top level page from a template
+    with open(templatefile) as htmlin:
+        # get list of all sensors in the cycledirs
+        sensors = []
+        for cycle in cycledirs:
+            dirs = glob.glob(os.path.join(cycle, '*'))
+            for sdir in dirs:
+                sensor = os.path.basename(sdir)
+                if sensor not in sensors:
+                    sensors.append(sensor)
+        # get list of rad, conv, oz sensors for menus
+        radlist = []
+        convlist = []
+        ozlist = []
+        for obtype in sensors:
+            if len(obtype.split('_')) > 1:
+                radlist.append(
+                    obtype) if obtype not in ozdiag else ozlist.append(obtype)
+            else:
+                convlist.append(obtype)
+        # create HTML to replace in template
+        rad_html = ''
+        conv_html = ''
+        oz_html = ''
+        for rad in radlist:
+            rad_html = rad_html + \
+                f'<a href="rad/{rad}/index.html">{rad.upper()}</a>\n'
+        for conv in convlist:
+            conv_html = conv_html + \
+                f'<a href="conv/{conv}/index.html">{conv.upper()}</a>\n'
+        for oz in ozlist:
+            oz_html = oz_html + \
+                f'<a href="oz/{oz}/index.html">{oz.upper()}</a>\n'
+        # find and replace in template
+        replacements = {
+            '{{EXPNAME}}': expname,
+            '{{RADLIST}}': rad_html,
+            '{{CONVLIST}}': conv_html,
+            '{{OZLIST}}': oz_html,
+            '{{CONTENTHTML}}': 'main.html',
+            '{{HOMEPATH}}': '',
+            '{{PANELH}}': '700px',
+        }
+        outfile = os.path.join(htmldir, 'index.html')
+        with open(outfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src, target)
+                htmlout.write(line)
+    return radlist, convlist, ozlist
+
+
+def gen_sensor_index(htmlfile, templatefile, expname, mysensor, radlist, convlist, ozlist):
+    # creates index.html for the individual ob type pages
+    with open(templatefile) as htmlin:
+        # create HTML to replace in template
+        rad_html = ''
+        conv_html = ''
+        oz_html = ''
+        for rad in radlist:
+            rad_html = rad_html + \
+                f'<a href="../../rad/{rad}/index.html">{rad.upper()}</a>\n'
+        for conv in convlist:
+            conv_html = conv_html + \
+                f'<a href="../../conv/{conv}/index.html">{conv.upper()}</a>\n'
+        for oz in ozlist:
+            oz_html = oz_html + \
+                f'<a href="../../oz/{oz}/index.html">{oz.upper()}</a>\n'
+        # find and replace in template
+        replacements = {
+            '{{EXPNAME}}': expname,
+            '{{RADLIST}}': rad_html,
+            '{{CONVLIST}}': conv_html,
+            '{{OZLIST}}': oz_html,
+            '{{CONTENTHTML}}': f'{mysensor}.html',
+            '{{HOMEPATH}}': '../../index.html',
+            '{{PANELH}}': '2000px',
+        }
+        with open(htmlfile, 'w') as htmlout:
+            for line in htmlin:
+                for src, target in replacements.items():
+                    line = line.replace(src, target)
+                htmlout.write(line)
+
+
+def gen_site(htmldir, templatedir, expname='evaltest'):
+    # generate HTML for DARTH site depending on what figures, etc. are in htmldir
+    # get list of cycles
+    cycledirs = glob.glob(os.path.join(htmldir, 'figs', '*'))
+    cycles = [os.path.basename(c) for c in cycledirs]
+    # just copy main.html if it does not exist
+    main_html = os.path.join(htmldir, 'main.html')
+    if os.path.exists(main_html):
+        os.remove(main_html)
+    shutil.copy(os.path.join(templatedir, 'main.html'), main_html)
+    # copy the javascript
+    main_js = os.path.join(htmldir, 'functions_main.js')
+    if os.path.exists(main_js):
+        os.remove(main_js)
+    shutil.copy(os.path.join(templatedir, 'functions_main.js'), main_js)
+    # create top level index.html
+    radlist, convlist, ozlist = gen_root_index(htmldir,
+                                             os.path.join(
+                                                 templatedir, 'index.html'),
+                                             cycledirs,
+                                             expname,
+                                             )
+    # now for each rad, conv, oz sensor, make a directory/html pages
+    for rad in radlist:
+        if not os.path.exists(os.path.join(htmldir, 'rad', rad)):
+            os.makedirs(os.path.join(htmldir, 'rad', rad))
+        gen_sensor_index(os.path.join(htmldir, 'rad', rad, 'index.html'),
+                       os.path.join(templatedir, 'index.html'),
+                       expname, rad, radlist, convlist, ozlist)
+        gen_rad_page(os.path.join(htmldir, 'rad', rad, f'{rad}.html'),
+                   os.path.join(templatedir, 'radmain.html'),
+                   cycles, cycledirs, rad)
+    for conv in convlist:
+        if not os.path.exists(os.path.join(htmldir, 'conv', conv)):
+            os.makedirs(os.path.join(htmldir, 'conv', conv))
+        gen_sensor_index(os.path.join(htmldir, 'conv', conv, 'index.html'),
+                       os.path.join(templatedir, 'index.html'),
+                       expname, conv, radlist, convlist, ozlist)
+        gen_conv_page(os.path.join(htmldir, 'conv', conv, f'{conv}.html'),
+                    os.path.join(templatedir, 'convmain.html'),
+                    cycles, cycledirs, conv)
+    for oz in ozlist:
+        if not os.path.exists(os.path.join(htmldir, 'oz', oz)):
+            os.makedirs(os.path.join(htmldir, 'oz', oz))
+        gen_sensor_index(os.path.join(htmldir, 'oz', oz, 'index.html'),
+                       os.path.join(templatedir, 'index.html'),
+                       expname, oz, radlist, convlist, ozlist)
---
+++ diags/procCycle.py
@@ -0,0 +1,216 @@
+from sklearn.linear_model import LinearRegression
+from scipy.interpolate import interpn
+from matplotlib.colors import Normalize
+from matplotlib import rcParams, ticker, cm
+import matplotlib.pyplot as plt
+from solo.date import Hour, DateIncrement
+from solo.logger import Logger
+from solo.configuration import Configuration
+from solo.basic_files import mkdir
+from r2d2 import fetch, date_sequence
+import glob
+import os
+import DARTHsite
+from multiprocessing import Pool
+import ioda
+import numpy as np
+import matplotlib
+import pandas as pd
+matplotlib.use('agg')
+
+logger = Logger('proc_cycle')
+config = Configuration('proc_cycle.yaml')
+nprocs = 40
+ozdiag = ['omps_npp']  # fill out later
+
+# set figure params one time only.
+rcParams['figure.subplot.left'] = 0.1
+rcParams['figure.subplot.top'] = 0.85
+rcParams['legend.fontsize'] = 12
+rcParams['axes.grid'] = True
+
+
+def proc_ioda_diag(config, diagpath):
+    # read, plot, save output from IODA diag file
+    # open IODA file and obs group
+    diag = ioda.Engines.HH.openFile(
+        name=diagpath,
+        mode=ioda.Engines.BackendOpenModes.Read_Only)
+    dlp = ioda._ioda_python.DLP.DataLayoutPolicy.generate(
+        ioda._ioda_python.DLP.DataLayoutPolicy.Policies(0))
+    og = ioda.ObsGroup(diag, dlp)
+    # get name of platform, etc.
+    diagpathbase = os.path.basename(diagpath).split('_')
+    obstype = '_'.join(diagpathbase[0:diagpathbase.index('hofx')])
+    if len(obstype.split('_')) > 1:
+        # either radiance or ozone
+        diagtype = 'oz' if obstype in ozdiag else 'conv'
+    else:
+        diagtype = 'conv'
+    # get list of variables in file
+    varlist = og.vars.list()
+    obsvarlist = []
+    for v in varlist:
+        vsplit = v.split('@')  # will probably need to change this later
+        try:
+            if vsplit[1] == 'hofx':
+                obsvarlist.append(vsplit[0])
+        except IndexError:
+            pass
+    # for each variable type, now create figures and make stats
+    # TODO fix this when brightness temp is 2D
+    for v in obsvarlist:
+        getvars = [
+            f'{v}@hofx',
+            f'{v}@GsiHofXBc',
+            f'{v}@EffectiveQC',
+            f'{v}@PreQC',
+            f'{v}@EffectiveError',
+            f'{v}@GsiFinalObsError',
+        ]
+        nlocs = np.arange(1, len(get_ioda_var(og, f'{v}@hofx'))+1)
+        dfDiag = pd.DataFrame(nlocs, columns=['nlocs'])
+        for gv in getvars:
+            dfDiag[gv] = get_ioda_var(og, gv)
+        # create scatter plot of H(x)
+        qc = dfDiag[dfDiag[f'{v}@GsiFinalObsError'].notnull()]
+        tmp = qc[[f'{v}@hofx', f'{v}@GsiHofXBc']].dropna()
+        outfig = os.path.join(config.stage,
+                              'DARTH', 'html', 'figs',
+                              Hour(config.cycle).format('%Y%m%d%H'),
+                              f'{obstype}_hofx_{v}_scatter.png',
+                              )
+        scatter_mdata = {
+            'title': f'{obstype} {v} H(x) Comparison',
+            'cycle': f'{config.cycle}',
+            'xlabel': 'GSI H(x)',
+            'ylabel': 'UFO H(x)',
+            'outfig': outfig,
+        }
+        gen_scatter(tmp[f'{v}@GsiHofXBc'], tmp[f'{v}@hofx'], scatter_mdata)
+
+
+def get_ioda_var(og, vname):
+    # get IODA var and return as numpy array
+    iodaVar = og.vars.open(vname)
+    if iodaVar.isA2(ioda._ioda_python.Types.float):
+        fillVal = iodaVar.atts.open('_FillValue').readDatum.float()
+        vdata = iodaVar.readNPArray.float()
+        vdata[vdata == fillVal] = np.nan
+    elif iodaVar.isA2(ioda._ioda_python.Types.int):
+        vdata = iodaVar.readNPArray.int()
+    else:
+        raise TypeError("Only float and int supported for now")
+    return vdata
+
+
+def fetch_diags(config):
+    # fetch diags from R2D2 to a working directory
+    window_start = Hour(config.cycle) - DateIncrement('PT3H')
+    ymdh = Hour(config.cycle).format('%Y%m%d%H')
+    stagedir = f'{config.stage}/diags/{ymdh}/'
+    mkdir(stagedir)
+    fetch(
+        type='diag',
+        model=config.model,
+        experiment=config.experiment,
+        date=window_start,
+        obs_type=config.obs_types,
+        database=config.database,
+        target_file=f'{stagedir}/$(obs_type)_hofx_{config.model}_$(date).nc4',
+    )
+
+
+def _get_linear_regression(data1, data2):
+    """
+    Inputs:
+        data1 : data on the x axis
+        data2 : data on the y axis
+    """
+    x = np.array(data1).reshape((-1, 1))
+    y = np.array(data2)
+    model = LinearRegression().fit(x, y)
+    r_sq = model.score(x, y)
+    intercept = model.intercept_
+    slope = model.coef_[0]
+    # This is the same as if you calculated y_pred
+    # by y_pred = slope * x + intercept
+    y_pred = model.predict(x)
+    return y_pred, r_sq, intercept, slope
+
+
+def gen_lineplot(dfX, dfY, metadata):
+    # generate and save line plot
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    plt.plot(dfX, dfY, linestyle=metadata['linestyle'], linewidth=metadata['linewidth'],
+             color=metadata['color'], label=metadata['label'])
+    plt.legend(loc='upper left', fontsize=11)
+    plt.title(metadata['title'], loc='left')
+    plt.xlabel(metadata['xlabel'], fontsize=12)
+    plt.ylabel(metadata['ylabel'], fontsize=12)
+    plt.title(metadata['cycle'], loc='right', fontweight='semibold')
+    plt.savefig(metadata['outfig'], bbox_inches='tight', pad_inches=0.1)
+    plt.close('all')
+
+
+def gen_scatter(dfX, dfY, metadata):
+    # generate and save scatter plot
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    y_pred, r_sq, intercept, slope = _get_linear_regression(dfX, dfY)
+    #plt.scatter(x=dfX, y =dfY, s=4, color='darkgray', label=f'n={dfX.count()}')
+    density_scatter(dfX.values, dfY.values, ax=ax, fig=fig,
+                    bins=[100, 100], s=4, cmap='hot')
+    label = f'y = {slope:.4f}x + {intercept:.4f}\nR\u00b2 : {r_sq:.4f}'
+    plt.plot(dfX, y_pred, color='black', linewidth=1, label=label)
+    plt.legend(loc='upper left', fontsize=11)
+    plt.title(metadata['title'], loc='left')
+    plt.xlabel(metadata['xlabel'], fontsize=12)
+    plt.ylabel(metadata['ylabel'], fontsize=12)
+    plt.title(metadata['cycle'], loc='right', fontweight='semibold')
+    plt.savefig(metadata['outfig'], bbox_inches='tight', pad_inches=0.1)
+    plt.close('all')
+
+
+def density_scatter(x, y, ax=None, fig=None, sort=True, bins=20, **kwargs):
+    """
+    Scatter plot colored by 2d histogram
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    data, x_e, y_e = np.histogram2d(x, y, bins=bins, density=True)
+    z = interpn((0.5*(x_e[1:] + x_e[:-1]), 0.5*(y_e[1:]+y_e[:-1])),
+                data, np.vstack([x, y]).T, method="splinef2d", bounds_error=False)
+    # To be sure to plot all data
+    z[np.where(np.isnan(z))] = 0.0
+    # Sort the points by density, so that the densest points are plotted last
+    if sort:
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+    ax.scatter(x, y, c=z, **kwargs)
+    norm = Normalize(vmin=np.min(z), vmax=np.max(z))
+    #cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)
+    # cbar.ax.set_ylabel('Density')
+    return ax
+
+
+def proc_cycle(config):
+    # process cycle of IODA diagnostic files
+    # use R2D2 to stage diag files
+    fetch_diags(config)
+    # get list of diag files to process
+    diagFiles = glob.glob(os.path.join(config.stage, 'diags',
+                                       Hour(config.cycle).format('%Y%m%d%H'),
+                                       '*_hofx_*'))
+    # TODO set up a multiprocessing pool to process each diag file in parallel
+    mkdir(os.path.join(config.stage, 'DARTH', 'html', 'figs',
+                       Hour(config.cycle).format('%Y%m%d%H')))
+    mkdir(os.path.join(config.stage, 'DARTH', 'html', 'stats',
+                       Hour(config.cycle).format('%Y%m%d%H')))
+    for diag in diagFiles:
+        proc_ioda_diag(config, diag)
+
+
+if __name__ == "__main__":
+    proc_cycle(config)
